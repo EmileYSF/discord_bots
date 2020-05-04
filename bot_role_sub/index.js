b52b97236d2151441de340b3dba29ec65d60be6a
meumz <emile.youssef94@gmail.com>
2020-05-05 00:52:08 +0200

    update

--- bot_role_sub/index.js
+++ bot_role_sub/index.js
@@ -1,24 +1,47 @@
+const fs = require('fs');
+const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 
-const Discord = require('discord.js');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', (message) => {
-    if (message.content.startsWith(`${prefix}ping`)) {
-        message.channel.send('Pong.');
-    } else if (message.content.startsWith(`${prefix}beep`)) {
-        message.channel.send('Boop.');
-    } else if (message.content === `${prefix}server`) {
-        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    } else if (message.content === `${prefix}user`) {
-        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+        return message.channel.send(reply);
     }
+    
+    try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 });
 
 client.login(token);
